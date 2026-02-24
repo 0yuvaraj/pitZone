f0ee0f67e4881037d15422fda9623c98cc4ea7fc
0yuvaraj <yuvaraju.oggu@gmail.com>
2026-02-24 11:35:26 +0530

    title changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'AttendWell',
+  title: 'pitZone',
   description: 'Your personal attendance advisor',
 };
 
